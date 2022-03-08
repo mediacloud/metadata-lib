42db8abad04ccd053e085a4f8146a0cc036c4dfb
Rahul Bhargava <rahulbot@gmail.com>
2022-03-08 10:54:12 -0500

    switch language detection for now
    Collaborators reported intalling cld2 library wrappers failed in containers, because it needs gcc to build cld2.

--- mcextractor/languages.py
+++ mcextractor/languages.py
@@ -1,12 +1,13 @@
 from typing import Optional
-import cld2
+import py3langid as langid
 import logging
 
 logger = logging.getLogger(__name__)
 
 
 def from_text(text: str) -> Optional[str]:
-    is_reliable, text_bytes_found, details = cld2.detect(text)
-    if is_reliable and len(details) > 0:
-        return details[0][1]
-    return None
+    try:
+        lang, prob = langid.classify(text)
+        return lang
+    except Exception as _:
+        return None
