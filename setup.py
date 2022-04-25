3b8d6b9792fa00036abe2450d05dde7bac59f6e9
Rahul Bhargava <rahulbot@gmail.com>
2022-04-25 11:03:33 -0400

    update dependencies and centralize - fix #20

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 #! /usr/bin/env python
 from setuptools import setup
 import re
-from os import path
+import os
+from pip._internal.network.session import PipSession
+from pip._internal.req import parse_requirements
 
 with open('mcmetadata/__init__.py', 'r') as fd:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE).group(1)
 
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md')) as f:
+# add README.md to distribution
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
+
+# load in centralized requirements (https://stackoverflow.com/a/57191701)
+requirements = parse_requirements(os.path.join(os.path.dirname(__file__), 'requirements.txt'), session=PipSession())
 
 setup(name='mediacloud-metadata',
       version=version,
@@ -19,8 +25,7 @@ setup(name='mediacloud-metadata',
       test_suite="mediacloud.test",
       packages=['mcmetadata'],
       package_data={'': ['LICENSE']},
-      install_requires=['requests', 'htmldate', 'dateparser', 'tldextract', 'py3langid', 'newspaper3k', 'goose3',
-                'BeautifulSoup4', 'readability-lxml', 'trafilatura', 'boilerpy3'],
+      install_requires=[str(requirement.requirement) for requirement in requirements],
       project_urls={  # Optional
               'Bug Reports': 'https://github.com/mediacloud/meta-extractor/issues',
               'Source': 'https://github.com/mediacloud/meta-extractor',
