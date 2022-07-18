34d4a2c3b7dbffc32b1f342f78334f3e8dae0adf
Rahul Bhargava <rahulbot@gmail.com>
2022-07-18 16:03:20 -0400

    more test cases

---
+++ mcmetadata/test/test_webpages.py
@@ -0,0 +1,32 @@
+import unittest
+import requests
+
+from .. import webpages
+
+
+class TestFetch(unittest.TestCase):
+
+    def test_regular_fetch(self):
+        url = "https://bostonglobe.com"
+        html, response = webpages.fetch(url)
+        assert response.status_code == 200
+        assert "Boston Globe" in html
+        assert response.encoding == "utf-8"
+
+    def test_non_utf8(self):
+        url = "https://www.mk.co.kr/news/society/view/2020/07/693939/"
+        html, response = webpages.fetch(url)
+        assert response.status_code == 200
+        assert response.encoding == "ISO-8859-1"
+
+    def test_bad_domain(self):
+        try:
+            url = "https://123_NO_DOIMAN"
+            _, _ = webpages.fetch(url)
+            assert False
+        except requests.exceptions.ConnectionError:
+            assert True
+
+
+if __name__ == "__main__":
+    unittest.main()
