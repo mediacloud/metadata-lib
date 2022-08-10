0ded5bbd5e1a91f888e10d4ace3a396c0c0dd453
Rahul Bhargava <rahulbot@gmail.com>
2022-08-10 16:27:59 -0400

    parameterize date test, add slow example too

--- mcmetadata/test/test_dates.py
+++ mcmetadata/test/test_dates.py
@@ -1,5 +1,6 @@
 import unittest
 import datetime as dt
+from parameterized import parameterized
 
 from .. import dates
 from .. import webpages
@@ -7,47 +8,34 @@ from .. import webpages
 
 class TestDates(unittest.TestCase):
 
-    def test_date_subhead(self):
-        u = "https://web.archive.org/web/https://www.sun-sentinel.com/community/fl-cn-calendar-events-20211201-20211129-sv3syeeuwzeallnr4vcanvaeti-story.html"
-        raw_html, response = webpages.fetch(u)
-        pub_date = dates.guess_publication_date(raw_html, u)
-        assert pub_date.date() == dt.date(2021, 11, 29)
-
-    def test_date_in_url(self):
-        url = "https://web.archive.org/web/https://www.cnn.com/2021/04/30/politics/mcconnell-1619-project-education-secretary/index.html"
-        raw_html, response = webpages.fetch(url)
+    @parameterized.expand([
+        # subhead
+        ("https://web.archive.org/web/https://www.sun-sentinel.com/community/fl-cn-calendar-events-20211201-20211129-sv3syeeuwzeallnr4vcanvaeti-story.html", dt.date(2021, 11, 29)),
+        # in url
+        ("https://web.archive.org/web/https://www.cnn.com/2021/04/30/politics/mcconnell-1619-project-education-secretary/index.html", dt.date(2021, 4, 30)),
+        # in meta tag
+        ("https://web.archive.org/web/https://www.foxnews.com/politics/biden-cancel-school-loans-corinthian-college-students", dt.date(2022, 6, 1)),
+        # ignore footer copyright
+        ("https://web.archive.org/web/https://www.alliedmarketresearch.com/cytogenetics-market", None),
+        ("https://web.archive.org/web/https://www.bakerbotts.com/footer/subscribe", None),
+        ("https://web.archive.org/web/https://www.kingjamesbibleonline.org/1-Chronicles-Chapter-1/", None),
+        ("https://web.archive.org/web/https://www.womblebonddickinson.com/us/people-search", None),
+        # ordered day month year
+        ("https://web.archive.org/web/https://www.eeas.europa.eu/eeas/eu-world-0_en", dt.date(2022, 3, 30)),
+        # undateable
+        ("http://archive.org", None),
+        # real site
+        ("https://web.archive.org/web/https://www.atv.hu/belfold/20190730/gyarfas-ugyvedje-aki-a-vadiratot-alkotta-nem-biztos-hogy-az-ugy-minden-reszletet-megismerte", dt.date(2019, 7, 30)),
+        # slow site
+        ("http://www.diariobahiadecadiz.com/noticias/san-fernando/cavada-y-romero-mantienen-otro-encuentro-con-defensa-para-negociar-la-desafectacion-de-suelo-en-camposoto-requerira-un-tiempo-indeterminado/", dt.date(2016, 10, 13))
+    ])
+    def test_pub_date(self, url, expected_date):
+        raw_html, response = webpages.fetch(url, timeout=120)
         pub_date = dates.guess_publication_date(raw_html, url)
-        assert pub_date.date() == dt.date(2021, 4, 30)
-
-    def test_date_in_meta_tags(self):
-        url = "https://web.archive.org/web/https://www.foxnews.com/politics/biden-cancel-school-loans-corinthian-college-students"
-        raw_html, response = webpages.fetch(url)
-        pub_date = dates.guess_publication_date(raw_html, url)
-        assert pub_date.date() == dt.date(2022, 6, 1)
-
-    def test_ignore_footer_copyright(self):
-        urls = [
-            "https://web.archive.org/web/https://www.alliedmarketresearch.com/cytogenetics-market",
-            "https://web.archive.org/web/https://www.bakerbotts.com/footer/subscribe",
-            "https://web.archive.org/web/https://www.kingjamesbibleonline.org/1-Chronicles-Chapter-1/",
-            "https://web.archive.org/web/https://www.womblebonddickinson.com/us/people-search"
-        ]
-        for u in urls:
-            raw_html, response = webpages.fetch(u)
-            date = dates.guess_publication_date(raw_html, u)
-            assert date is None
-
-    def test_day_month_year(self):
-        u = "https://web.archive.org/web/https://www.eeas.europa.eu/eeas/eu-world-0_en"
-        raw_html, response = webpages.fetch(u)
-        date = dates.guess_publication_date(raw_html, u)
-        assert date.date() == dt.date(2022, 3, 30)
-
-    def test_no_date(self):
-        u = "http://archive.org"
-        raw_html, response = webpages.fetch(u)
-        date = dates.guess_publication_date(raw_html, u)
-        assert date is None
+        if expected_date is None:
+            assert pub_date is None
+        else:
+            assert pub_date.date() == expected_date
 
     def test_max_date(self):
         u = "https://web.archive.org/web/https://www.canarias7.es/cultura/cimientos-artes-escenicas-20220718203045-nt.html"
@@ -56,9 +44,3 @@ class TestDates(unittest.TestCase):
         assert date.date() == dt.date(2022, 7, 17)
         date = dates.guess_publication_date(raw_html, u, max_date=dt.datetime(2020, 1, 1))
         assert date is None
-
-    def test_real_date1(self):
-        u = "https://web.archive.org/web/https://www.atv.hu/belfold/20190730/gyarfas-ugyvedje-aki-a-vadiratot-alkotta-nem-biztos-hogy-az-ugy-minden-reszletet-megismerte"
-        raw_html, response = webpages.fetch(u)
-        date = dates.guess_publication_date(raw_html, u)
-        assert date.date() == dt.date(2019, 7, 30)
