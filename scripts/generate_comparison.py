a31fe6a9c90c01f36729ac7cac2c1db9a296def8
Rahul Bhargava <rahulbot@gmail.com>
2022-08-10 15:08:43 -0400

    generate comparison CSV to compare results to prod system

--- scripts/generate_comparison.py
+++ scripts/generate_comparison.py
@@ -1,12 +1,22 @@
+"""
+Run URLs from our production system through the pipeline and save a CSV that lets us compare results to see if
+this code produces similar metadata to the system the heuristics were extracted from. Useful for validating that
+we are getting results we expect from the code as we evaluate different libraries and solutions.
+"""
+
 import csv
+import logging
+
 import dateparser
-from typing import Dict
+from typing import Dict, Optional
 import multiprocessing as mp
 
-from mcmetadata import extract
+import mcmetadata
+
+logger = logging.getLogger(__file__)
 
 
-def _comparison_worker(s: Dict) -> Dict:
+def _comparison_worker(s: Dict) -> Optional[Dict]:
     try:
         results = {
             'url': s['url'],
@@ -15,7 +25,7 @@ def _comparison_worker(s: Dict) -> Dict:
             'title': s['title'],
         }
         # guesses
-        info = extract(results['url'])
+        info = mcmetadata.extract(results['url'])
         results['language_guess'] = info['language']
         results['pub_date_guess'] = info['publication_date'].date()
         results['title_guess'] = info['article_title']
@@ -25,16 +35,19 @@ def _comparison_worker(s: Dict) -> Dict:
         results['pub_date_diff'] = (results['pub_date'] - results['pub_date_guess']).days
         results['title_match'] = results['title_guess'] == results['title']
         return results
-    except Exception as e:
+    except Exception:
         return None
 
 
-def generate_comparison_csv(input_file: str):
+def generate_comparison_csv(input_file: str, parallel: bool):
     with open(input_file) as infile:
         reader = csv.DictReader(infile)
         # run in parallel for performance improvement
-        pool = mp.Pool(processes=16)
-        results = [r for r in pool.map(_comparison_worker, [s for s in reader]) if r is not None]
+        if parallel:
+            pool = mp.Pool(processes=16)
+            results = [r for r in pool.map(_comparison_worker, [s for s in reader][:200]) if r is not None]
+        else:
+            results = [_comparison_worker(r) for r in reader if r is not None]
     # write out csv for manual review
     with open(input_file.split('.')[0]+"-comparison.csv", 'w') as outfile:
         cols = ['url', 'pub_date', 'pub_date_guess', 'pub_date_match', 'pub_date_diff',
@@ -47,4 +60,4 @@ def generate_comparison_csv(input_file: str):
 
 
 if __name__ == "__main__":
-    generate_comparison_csv('random-stories.csv')
+    generate_comparison_csv('random-stories.csv', True)
