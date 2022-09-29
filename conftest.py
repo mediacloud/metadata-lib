4a1377055c2155a069e4d498e9d8d5b41ab94d51
Paige Gulley <nano3.14@gmail.com>
2022-09-29 15:06:29 -0400

    moved conftest

---
+++ conftest.py
@@ -0,0 +1,8 @@
+import pytest
+from distutils.util import strtobool
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--use-cache', default=True, nargs="?", const=True, help='Use cached versions of content instead of fetching at every step',
+        type=lambda x:bool(strtobool(x))
+    )
--- mcmetadata/test/conftest.py
+++
@@ -1,6 +0,0 @@
-import pytest
-
-def pytest_addoption(parser):
-    parser.addoption(
-        '--use-cache', default=True, help='Use cached versions of content instead of fetching at every step'
-    )
